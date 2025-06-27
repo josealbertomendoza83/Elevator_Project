e2e54e88530297aff386106cc8cd37bfc030230e
Alberto Mendoza <josealbertomendoza83@gmail.com>
2025-06-26 22:07:37 -0600

    Agregar archivos de proyecto.

---
+++ Elevator.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Elevator_Project
+{
+    public class Elevator
+    {
+
+  
+        private bool[] floorReady;
+        public int CurrentFloor = 1;
+        private int topfloor;
+        public ElevatorStatus Status = ElevatorStatus.STOPPED;
+
+        public Elevator(int NumberOfFloors = 10)
+        {
+            floorReady = new bool[NumberOfFloors + 1];
+            topfloor = NumberOfFloors;
+        }
+
+        private void Stop(int floor)
+        {
+            Status = ElevatorStatus.STOPPED;
+            CurrentFloor = floor;
+            floorReady[floor] = false;
+            Console.WriteLine("Stopped at floor {0}", floor);
+        }
+
+        private void Descend(int floor)
+        {
+            for (int i = CurrentFloor; i >= 1; i--)
+            {
+                if (floorReady[i]) { 
+                    Console.WriteLine("Waiting - Descending..");
+                    Thread.Sleep(3000); //Simulating Descending
+                    Stop(floor);
+                }
+                else
+                    continue;
+            }
+            Status = ElevatorStatus.STOPPED;
+        }
+
+        private void Ascend(int floor)
+        {
+            for (int i = CurrentFloor; i <= topfloor; i++)
+            {
+                if (floorReady[i])
+                {
+                    Console.WriteLine("Waiting - Ascending..");
+
+                    Thread.Sleep(3000); //Simulating Descending
+                    Stop(floor);
+                }
+                else {
+                   // ProgressUtility.WriteProgressBar(i, true); 
+                   // ProgressUtility.WriteProgress(i, true);
+                    continue;
+                   
+                }
+                    
+            }
+
+           
+            Status = ElevatorStatus.STOPPED;
+        }
+
+        void StayPut()
+        {
+            Console.WriteLine("That's our current floor");
+        }
+
+        public void FloorPress(int floor)
+        {
+            if (floor > topfloor)
+            {
+                Console.WriteLine("We only have {0} floors", topfloor);
+                return;
+            }
+
+            floorReady[floor] = true;
+
+            switch (Status)
+            {
+
+                case ElevatorStatus.DOWN:
+                    Descend(floor);
+                    break;
+
+                case ElevatorStatus.STOPPED:
+                    if (CurrentFloor < floor)
+                        Ascend(floor);
+                    else if (CurrentFloor == floor)
+                        StayPut();
+                    else
+                        Descend(floor);
+                    break;
+
+                case ElevatorStatus.UP:
+                    Ascend(floor);
+                    break;
+
+                default:
+                    break;
+            }
+
+
+        }
+
+        public enum ElevatorStatus
+        {
+            UP,
+            STOPPED,
+            DOWN
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Elevator_Project
+{
+    internal class Program
+    {
+        private const string QUIT = "q";
+        public static void Main(string[] args)
+        {
+
+        Start:
+            Console.WriteLine("Welcome to GP elevator!!");
+            Console.WriteLine("Please type how many floors this building will be in?");
+
+            int floor; string floorInput; Elevator elevator;
+
+            floorInput = Console.ReadLine();
+
+            if (Int32.TryParse(floorInput, out floor))
+                elevator = new Elevator(floor);
+            else
+            {
+                Console.WriteLine("That' doesn't make sense...");
+                Console.Beep();
+                Thread.Sleep(2000);
+                Console.Clear();
+                goto Start;
+            }
+            string input = string.Empty;
+
+            while (input != QUIT)
+            {
+                Console.WriteLine("Please press which floor you would like to go to");
+
+                input = Console.ReadLine();
+                if (Int32.TryParse(input, out floor))
+                    elevator.FloorPress(floor);
+                else if (input == QUIT)
+                    Console.WriteLine("GoodBye!");
+                else
+                    Console.WriteLine("You have pressed an incorrect floor, Please try again");
+            }
+        }
+
+    }
+    
+}
